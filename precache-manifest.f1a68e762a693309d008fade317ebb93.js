803c583cb3c04ad20e782c2fc4f5cb0e34c1c321
Hemang Bhogayata <hemang249@gmail.com>
2020-01-30 17:16:30 +0530

    Updates

---
+++ precache-manifest.f1a68e762a693309d008fade317ebb93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6892b47b22db32bb176ad4dd6247fb2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd4d1aa059ec24d6db9",
+    "url": "/static/css/main.74544581.chunk.css"
+  },
+  {
+    "revision": "cf11fbbfc1f115395512",
+    "url": "/static/js/2.c8123600.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.c8123600.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5cd4d1aa059ec24d6db9",
+    "url": "/static/js/main.7f0add75.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "7612091a51d9ed65e7bb877a4ad919f2",
+    "url": "/static/media/contact.7612091a.png"
+  },
+  {
+    "revision": "2f6a319964974c9c5f2b3c1a72350fa0",
+    "url": "/static/media/deploy.2f6a3199.png"
+  },
+  {
+    "revision": "f0c0aafb3f3f7cee043a21f28fe62ab7",
+    "url": "/static/media/me.f0c0aafb.png"
+  }
+]);
